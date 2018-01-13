d4487f024165ac3aae4e89e1afe16cf8b516a759
Anthony Damico <ajdamico@gmail.com>
2018-01-13 07:23:23 -0500

    savecache=FALSE inside syntaxtractor

--- tests/setup.R
+++ tests/setup.R
@@ -1,5 +1,7 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+options("lodown.cachaca.savecache"=FALSE)
+
 library(lodown)
 this_sample_break <- Sys.getenv( "this_sample_break" )
 timss_cat <- get_catalog( "timss" , output_dir = file.path( getwd() ) )

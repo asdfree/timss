bc74ac2509eba72004c598c4843fc79d1ea76047
Anthony Damico <ajdamico@gmail.com>
2023-07-11 12:59:47 -0400

    timss draft

--- tests/setup.R
+++ tests/setup.R
@@ -50,9 +50,12 @@ GET( this_url , write_disk( tf ) , progress() )
 unzipped_files <- unzip( tf , exdir = tempdir() )
 library(haven)
 
+# limit unzipped files to those starting with `asg` followed by three letters followed by `m7`
+asg_fns <- unzipped_files[ grepl( '^asg[a-z][a-z][a-z]m7' , basename( unzipped_files ) ) ]
+
 timss_df <- NULL
 
-for( spss_fn in unzipped_files[ grepl( '^asg[a-z][a-z][a-z]m7' , basename( unzipped_files ) ) ] ){
+for( spss_fn in asg_fns ){
 
 	this_tbl <- read_spss( spss_fn )
 	
@@ -66,32 +69,40 @@ for( spss_fn in unzipped_files[ grepl( '^asg[a-z][a-z][a-z]m7' , basename( unzip
 	
 }
 
+# order the data.frame by unique student id
 timss_df <- timss_df[ with( timss_df , order( idcntry , idstud ) ) , ]
 # timss_fn <- file.path( path.expand( "~" ) , "TIMSS" , "this_file.rds" )
 # saveRDS( timss_df , file = timss_fn , compress = FALSE )
 # timss_df <- readRDS( timss_fn )
+# identify all columns ending with `01` thru `05`
 ppv <- grep( "(.*)0[1-5]$" , names( timss_df ) , value = TRUE )
+
+# remove those ending digits
 ppv_prefix <- gsub( "0[1-5]$" , "" , ppv )
+
+# identify each of the possibilities with exactly five matches (five implicates)
 pv <- names( table( ppv_prefix )[ table( ppv_prefix ) == 5 ] )
-pv_columns <- grep( paste0( "^" , pv , "0[1-5]$" , collapse = "|" ) , names( timss_df ) , value = TRUE )
 
-id_columns <- c( 'idcntry' , 'idstud' )
-
-pv_wide_df <- timss_df[ c( id_columns , pv_columns ) ]
+# identify each of the `01` thru `05` plausible value columns
+pv_columns <-
+	grep( 
+		paste0( "^" , pv , "0[1-5]$" , collapse = "|" ) , 
+		names( timss_df ) , 
+		value = TRUE 
+	)
+pv_wide_df <- timss_df[ c( 'idcntry' , 'idstud' , pv_columns ) ]
 
 timss_df[ pv_columns ] <- NULL
-
 pv_long_df <- 
 	reshape( 
 		pv_wide_df , 
 		varying = lapply( paste0( pv , '0' ) , paste0 , 1:5 ) , 
 		direction = 'long' , 
 		timevar = 'implicate' , 
-		idvar = id_columns 
+		idvar = c( 'idcntry' , 'idstud' ) 
 	)
 
 names( pv_long_df ) <- gsub( "01$" , "" , names( pv_long_df ) )
-
 timss_long_df <- merge( timss_df , pv_long_df )
 
 timss_long_df <- timss_long_df[ with( timss_long_df , order( idcntry , idstud ) ) , ]
@@ -133,11 +144,22 @@ timss_design <-
 		
 		one = 1 ,
 		
-		idcntry = factor( idcntry ) ,
+		countries_thru_canada = 
 		
-		sex = factor( itsex , labels = c( "male" , "female" ) ) ,
+			factor( 
+				idcntry ,
+				
+				levels = c(8L, 51L, 36L, 40L, 31L, 48L, 956L, 70L, 100L, 124L) ,
+
+				labels =
+					c("Albania", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahrain",
+					"Belgium (Flemish)", "Bosnia and Herzegovina", "Bulgaria", "Canada")
+				
+			) ,
 		
-		born_2005_or_later = as.numeric( itbirthy >= 2005 )
+		sex = factor( asbg01 , levels = 1:2 , labels = c( "female" , "male" ) ) ,
+		
+		born_in_country = ifelse( asbg07 %in% 1:2 , as.numeric( asbg07 == 1 ) , NA )
 
 	)
 timss_MIcombine( with( timss_design , svyby( ~ one , ~ one , unwtd.count ) ) )
@@ -153,20 +175,20 @@ timss_MIcombine( with( timss_design , svymean( ~ asmmat ) ) )
 timss_MIcombine( with( timss_design ,
 	svyby( ~ asmmat , ~ sex , svymean )
 ) )
-timss_MIcombine( with( timss_design , svymean( ~ idcntry ) ) )
+timss_MIcombine( with( timss_design , svymean( ~ countries_thru_canada ) ) )
 
 timss_MIcombine( with( timss_design ,
-	svyby( ~ idcntry , ~ sex , svymean )
+	svyby( ~ countries_thru_canada , ~ sex , svymean )
 ) )
 timss_MIcombine( with( timss_design , svytotal( ~ asmmat ) ) )
 
 timss_MIcombine( with( timss_design ,
 	svyby( ~ asmmat , ~ sex , svytotal )
 ) )
-timss_MIcombine( with( timss_design , svytotal( ~ idcntry ) ) )
+timss_MIcombine( with( timss_design , svytotal( ~ countries_thru_canada ) ) )
 
 timss_MIcombine( with( timss_design ,
-	svyby( ~ idcntry , ~ sex , svytotal )
+	svyby( ~ countries_thru_canada , ~ sex , svytotal )
 ) )
 timss_MIcombine( with( timss_design ,
 	svyquantile(
@@ -183,7 +205,7 @@ timss_MIcombine( with( timss_design ,
 timss_MIcombine( with( timss_design ,
 	svyratio( numerator = ~ asssci , denominator = ~ asmmat )
 ) )
-sub_timss_design <- subset( timss_design , idcntry %in% c( 36 , 40 , 31 , 957 ) )
+sub_timss_design <- subset( timss_design , idcntry %in% c( 36 , 40 , 31 , 956 ) )
 timss_MIcombine( with( sub_timss_design , svymean( ~ asmmat ) ) )
 this_result <-
 	timss_MIcombine( with( timss_design ,
@@ -215,37 +237,41 @@ timss_MIcombine( with( timss_design ,
 timss_MIcombine( with( timss_design ,
 	svymean( ~ asmmat , deff = "replace" )
 ) )
-# MIsvyciprop( ~ born_2001_or_later , timss_design ,
+# MIsvyciprop( ~ born_in_country , timss_design ,
 # 	method = "likelihood" , na.rm = TRUE )
-# MIsvyttest( asmmat ~ born_2001_or_later , timss_design )
-# MIsvychisq( ~ born_2001_or_later + idcntry , timss_design )
+# MIsvyttest( asmmat ~ born_in_country , timss_design )
+# MIsvychisq( ~ born_in_country + countries_thru_canada , timss_design )
 glm_result <- 
 	timss_MIcombine( with( timss_design ,
-		svyglm( asmmat ~ born_2001_or_later + idcntry )
+		svyglm( asmmat ~ born_in_country + countries_thru_canada )
 	) )
 	
 summary( glm_result )
-timss_design <- update( timss_design , one = 1 )
-w <- subset( timss_design , idcntry == 36 )
-nrow(w)
-lapply( w$designs , function( u ) vcov( svymean( ~ asmmat , u ) ) )
-mean( unlist( lapply( w$designs , function( u ) vcov( svymean( ~ asmmat , u ) ) ) ) )
-timss_MIcombine( with( w , svymean( ~ asmmat ) ) )
+australia_design <- subset( timss_design , countries_thru_canada == "Australia" )
 
-library(haven)
+stopifnot( nrow( australia_design ) == 5890 )
 
-this_df <- read_spss( grep( 'asgaus' , unzipped_files , value = TRUE ) )
-this_df <- zap_labels( this_df )
-this_df <- data.frame( this_df )
-names( this_df ) <- tolower( names( this_df ) )
+result <- timss_MIcombine( with( australia_design , svymean( ~ asmmat ) ) )
 
-mean( c(
-	with( this_df , weighted.mean( asmmat01 , totwgt ) ) ,
-	with( this_df , weighted.mean( asmmat02 , totwgt ) ) ,
-	with( this_df , weighted.mean( asmmat03 , totwgt ) ) ,
-	with( this_df , weighted.mean( asmmat04 , totwgt ) ) ,
-	with( this_df , weighted.mean( asmmat05 , totwgt ) )
-) )
+stopifnot( round( coef( result ) , 3 ) == 515.880 )
+
+stopifnot( round( SE( result ) , 3 ) == 2.776 )
+
+australia_tbl <- read_spss( grep( 'asgaus' , unzipped_files , value = TRUE ) )
+australia_tbl <- zap_labels( australia_tbl )
+australia_df <- data.frame( australia_tbl )
+names( australia_df ) <- tolower( names( australia_df ) )
+
+mean_proficiency <-
+	mean( c(
+		with( australia_df , weighted.mean( asmmat01 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat02 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat03 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat04 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat05 , totwgt ) )
+	) )
+
+stopifnot( round( mean_proficiency , 3 ) == 515.880 )
 
 for( k in 1:5 ){
 
@@ -257,12 +283,16 @@ for( k in 1:5 ){
 				this_variance + 
 				( 
 					weighted.mean( 
-						this_df[ , paste0( 'asmmat0' , k ) ] , 
-						ifelse( j == this_df[ , 'jkzone' ] , this_df[ , 'totwgt' ] * 2 * ( this_df[ , 'jkrep' ] == i ) , this_df[ , 'totwgt' ] )
+						australia_df[ , paste0( 'asmmat0' , k ) ] , 
+						ifelse( 
+							j == australia_df[ , 'jkzone' ] , 
+							australia_df[ , 'totwgt' ] * 2 * ( australia_df[ , 'jkrep' ] == i ) , 
+							australia_df[ , 'totwgt' ] 
+						)
 					) -
 					weighted.mean( 
-						this_df[ , paste0( 'asmmat0' , k ) ] , 
-						this_df[ , 'totwgt' ]
+						australia_df[ , paste0( 'asmmat0' , k ) ] , 
+						australia_df[ , 'totwgt' ]
 					)
 				)^2
 		}
@@ -272,25 +302,29 @@ for( k in 1:5 ){
 
 }
 
-# sampling variance
-mean( c( v1 , v2 , v3 , v4 , v5 ) )
+sampling_variance <- mean( c( v1 , v2 , v3 , v4 , v5 ) )
+stopifnot( round( sampling_variance , 3 ) == 7.397 )
 
 t0 <-
 	mean( c(
-		with( this_df , weighted.mean( asmmat01 , totwgt ) ) ,
-		with( this_df , weighted.mean( asmmat02 , totwgt ) ) ,
-		with( this_df , weighted.mean( asmmat03 , totwgt ) ) ,
-		with( this_df , weighted.mean( asmmat04 , totwgt ) ) ,
-		with( this_df , weighted.mean( asmmat05 , totwgt ) )
+		with( australia_df , weighted.mean( asmmat01 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat02 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat03 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat04 , totwgt ) ) ,
+		with( australia_df , weighted.mean( asmmat05 , totwgt ) )
 	) )
 
-# imputation variance
-( 6 / 5 ) * 
-	( 
-		( ( with( this_df , weighted.mean( asmmat01 , totwgt ) ) - t0 )^2 / 4 ) +
-		( ( with( this_df , weighted.mean( asmmat02 , totwgt ) ) - t0 )^2 / 4 ) +
-		( ( with( this_df , weighted.mean( asmmat03 , totwgt ) ) - t0 )^2 / 4 ) +
-		( ( with( this_df , weighted.mean( asmmat04 , totwgt ) ) - t0 )^2 / 4 ) +
-		( ( with( this_df , weighted.mean( asmmat05 , totwgt ) ) - t0 )^2 / 4 ) 
-	)
+imputation_variance <-
+	( 6 / 5 ) * 
+		( 
+			( ( with( australia_df , weighted.mean( asmmat01 , totwgt ) ) - t0 )^2 / 4 ) +
+			( ( with( australia_df , weighted.mean( asmmat02 , totwgt ) ) - t0 )^2 / 4 ) +
+			( ( with( australia_df , weighted.mean( asmmat03 , totwgt ) ) - t0 )^2 / 4 ) +
+			( ( with( australia_df , weighted.mean( asmmat04 , totwgt ) ) - t0 )^2 / 4 ) +
+			( ( with( australia_df , weighted.mean( asmmat05 , totwgt ) ) - t0 )^2 / 4 ) 
+		)
+
+stopifnot( round( imputation_variance , 3 ) == 0.309 )
+
+stopifnot( round( sampling_variance + imputation_variance , 3 ) == 7.706 )
 
